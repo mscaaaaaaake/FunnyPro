e3b395291654649b898874d9efa372edef606400
123 <1107540794@qq.com>
2018-09-04 15:29:36 +0800

    ~

--- src/main/java/com/funny/controller/FunnyController.java
+++ src/main/java/com/funny/controller/FunnyController.java
@@ -25,11 +25,9 @@ public class FunnyController extends HttpServlet{
         writer.println("<html><body>");
         if (account == null || password == null) {
             writer.println("<p>miss account or password</p>");
-            writer.println("<p>hao e</p>");
         }else{
             if(account.equals("admin") && password.equals("123456")) {
                 writer.println("<h2>welcome,"+account+"</h2>");
-                writer.println("<p>xiangchirou</p>");
             }else{
                 writer.println("<p>you got the wrong account or password</p>");
             }
